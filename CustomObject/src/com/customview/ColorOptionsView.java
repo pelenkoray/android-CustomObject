025e6fe4bd27aadc0e22480a233e9b4504b56f59
Koray Pelen <koray_pelen@hotmail.com>
2014-09-22 15:50:11 +0300

    v3

--- CustomObject/src/com/customview/ColorOptionsView.java
+++ CustomObject/src/com/customview/ColorOptionsView.java
@@ -14,47 +14,49 @@ import android.widget.TextView;
 
 public class ColorOptionsView extends LinearLayout {
 
-  private View mValue;
-  private ImageView mImage;
+	private View mValue;
+	private ImageView mImage;
 
-  public ColorOptionsView(Context context, AttributeSet attrs) {
-    super(context, attrs);
+	public ColorOptionsView(Context context, AttributeSet attrs) {
+		super(context, attrs);
 
-    TypedArray a = context.obtainStyledAttributes(attrs,
-        R.styleable.Options, 0, 0);
-    String titleText = a.getString(R.styleable.Options_titleText);
-    int valueColor = a.getColor(R.styleable.Options_valueColor,
-        android.R.color.holo_blue_light);
-    int resId=a.getResourceId(R.styleable.Options_valueURL, android.R.drawable.ic_menu_help);
-    a.recycle();
+		TypedArray a = context.obtainStyledAttributes(attrs,
+				R.styleable.Options, 0, 0);
+		String titleText = a.getString(R.styleable.Options_titleText);
+		int valueColor = a.getColor(R.styleable.Options_valueColor,
+				android.R.color.holo_blue_light);
+		int resId = a.getResourceId(R.styleable.Options_valueURL,
+				android.R.drawable.ic_menu_help);
+		a.recycle();
 
-    setOrientation(LinearLayout.HORIZONTAL);
-    setGravity(Gravity.CENTER_VERTICAL);
+		setOrientation(LinearLayout.HORIZONTAL);
+		setGravity(Gravity.CENTER_VERTICAL);
 
-    LayoutInflater inflater = (LayoutInflater) context
-        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    inflater.inflate(R.layout.view_color_options, this, true);
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.view_color_options, this, true);
 
-    TextView title = (TextView) getChildAt(0);
-    title.setText(titleText);
+		TextView title = (TextView) getChildAt(0);
+		title.setText(titleText);
 
-    mValue = getChildAt(1);
-    mValue.setBackgroundColor(valueColor);
+		mValue = getChildAt(1);
+		//mValue.setBackgroundColor(valueColor);
+		setValueColor(valueColor);
+		mImage = (ImageView) getChildAt(2);
+		mImage.setImageResource(resId);
+		setImageVisible(true);
+	}
 
-    mImage = (ImageView) getChildAt(2);
-    mImage.setImageResource(resId);
-  }
+	public ColorOptionsView(Context context) {
+		this(context, null);
+	}
 
-  public ColorOptionsView(Context context) {
-    this(context, null);
-  }
+	public void setValueColor(int color) {
+		mValue.setBackgroundColor(color);
+	}
 
-  public void setValueColor(int color) {
-    mValue.setBackgroundColor(color);
-  }
+	public void setImageVisible(boolean visible) {
+		mImage.setVisibility(visible ? View.VISIBLE : View.GONE);
+	}
 
-  public void setImageVisible(boolean visible) {
-    mImage.setVisibility(visible ? View.VISIBLE : View.GONE);
-  }
-
-} 
+}
